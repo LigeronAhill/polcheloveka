74d03baff7457d1b964923ac985030b7bba46e98
Aleksandr Provotorov <provotorov@inbox.ru>
2025-10-07 10:45:28 +0300

    filters update

--- constants/filters.ts
+++ constants/filters.ts
@@ -6,9 +6,9 @@ export const AnswerFilters = [
 ];
 
 export const UserFilters = [
-  { name: "New Users", value: "new_users" },
-  { name: "Old Users", value: "old_users" },
-  { name: "Top Contributors", value: "top_contributors" },
+  { name: "Новые пользователи", value: "new_users" },
+  { name: "Ветераны", value: "old_users" },
+  { name: "Самые активные", value: "top_contributors" },
 ];
 
 export const QuestionFilters = [
