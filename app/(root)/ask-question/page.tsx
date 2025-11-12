8f282f2610efcd08db870208ffc729a01a251316
Aleksandr Provotorov <provotorov@inbox.ru>
2025-11-12 15:47:22 +0300

    ask question error

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,28 +1,21 @@
-import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 import Question from "@/components/forms/question";
-import { getUserById } from "@/lib/actions/user.action";
-import { auth } from "@/lib/auth";
+import { getUserProfile } from "@/lib/actions/user.action";
 
 export default async function AskQuestion() {
-	const session = await auth.api.getSession({
-		headers: await headers(),
-	});
-	const userId = session?.user?.id;
-	if (!userId) {
+	const user = await getUserProfile();
+	if (!user) {
 		console.log("not allowed");
 		redirect("/login");
 	}
 
-	const mongoUser = await getUserById(userId);
-	const uid = JSON.stringify(mongoUser._id);
 	return (
 		<div>
 			<h1 className="font-bold text-3xl text-dark-100 dark:text-light-900">
 				Задать вопрос
 			</h1>
 			<div className="mt-9">
-				<Question mongoUserId={uid} type="create" />
+				<Question mongoUserId={user._id.toString()} type="create" />
 			</div>
 		</div>
 	);
