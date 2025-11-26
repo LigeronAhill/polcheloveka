b93f920131e3eaedefdab39abf04c67f0fbdfa1c
LigeronAhill <provotorov@inbox.ru>
2025-11-26 16:21:32 +0300

    local searching in home, community, tags, collection

--- components/shared/questionTab.tsx
+++ components/shared/questionTab.tsx
@@ -1,42 +1,42 @@
-import { getUserQuestions } from "@/lib/actions/user.action";
-import type { SearchParamsProps } from "@/types";
+import {getUserQuestions} from "@/lib/actions/user.action";
 import QuestionCard from "./cards/questionCard";
 import Pagination from "./pagination";
 
-interface Props extends SearchParamsProps {
-	userId: string;
+interface Props {
+    searchParams: { [key: string]: string | undefined };
+    userId: string;
 }
 
-const QuestionTab = async ({ searchParams, userId }: Props) => {
-	const result = await getUserQuestions({
-		userId,
-		page: searchParams.page ? +searchParams.page : 1,
-	});
+const QuestionTab = async ({searchParams, userId}: Props) => {
+    const result = await getUserQuestions({
+        userId,
+        page: searchParams.page ? +searchParams.page : 1,
+    });
 
-	return (
-		<>
-			{result.questions.map((question) => (
-				<QuestionCard
-					key={question._id}
-					_id={question._id}
-					title={question.title}
-					tags={question.tags}
-					author={question.author}
-					upvotes={question.upvotes.length}
-					views={question.views}
-					answers={question.answers.length}
-					createdAt={question.createdAt}
-				/>
-			))}
+    return (
+        <>
+            {result.questions.map((question) => (
+                <QuestionCard
+                    key={question._id}
+                    _id={question._id}
+                    title={question.title}
+                    tags={question.tags}
+                    author={question.author}
+                    upvotes={question.upvotes.length}
+                    views={question.views}
+                    answers={question.answers.length}
+                    createdAt={question.createdAt}
+                />
+            ))}
 
-			<div className="mt-10">
-				<Pagination
-					pageNumber={searchParams?.page ? +searchParams.page : 1}
-					isNext={result.isNextQuestions}
-				/>
-			</div>
-		</>
-	);
+            <div className="mt-10">
+                <Pagination
+                    pageNumber={searchParams?.page ? +searchParams.page : 1}
+                    isNext={result.isNextQuestions}
+                />
+            </div>
+        </>
+    );
 };
 
 export default QuestionTab;
